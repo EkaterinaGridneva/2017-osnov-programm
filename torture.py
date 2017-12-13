a8db43ed2b82bb721ead03474c7eac5b5472aba8
EkaterinaGridneva <toki91@mail.ru>
2017-12-13 18:18:48 +0000

    Erg

--- torture.py
+++ torture.py
@@ -1,5 +1,5 @@
 import sys 
-
+count=[]
 dot=[] # list of english words
 fd=open('dot', 'r')
 # read in list of english words
@@ -8,7 +8,22 @@ for line in fd.readlines():
          dot.append (line)
 
 # for line in the german corpus
+Corpusde=[]
+fd=open ('Corpus.de.txt.txt','r')
+# read in list of german words
+for line in fd.readlines():
+	line=line.strip()
+	Corpusde.append (line)
 	# tokenise the line
+	row = newline.split(' ')
 	# for each of the tokens
+	for token in row:
+		if token != '':
 		# check if it is in the english wordlist
+		if token in dot:
+			print (token)
 		# if it is, take a count
+
+			count.append (token)
+					
+print (count)
